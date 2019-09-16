59fceee80b1e531cff7d1510ca78d3a890e7d442
Julian Irigoyen <julian.irigoyen@synapse.com>
2019-09-16 11:50:31 -0300

    Added user and user profile deletion logic

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,7 +27,7 @@ router.get('/me', auth, async (req, res) => {
 });
 
 //@route POST api/profile
-//@desc Create or update a user profuile
+//@desc Create or update a user profile
 //@access Private - Auth required
 router.post(
   '/',
@@ -109,5 +109,59 @@ router.post(
     }
   }
 );
+
+//@route GET api/profile
+//@desc Get all profiles
+//@access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by id
+//@access Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }) //user_id is coming from URL so check the Request (req) for it
+      .populate('user', ['name', 'avatar']);
+
+    //Check for profile existence
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+  }
+});
+
+//@route DELETE api/profile
+//@desc Delete profile, user & posts
+//@access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //Remove user profile
+    await Profile.findOneAndRemove({
+      user: req.user.id
+    }); //being this route private, there is access to auth middleware, so there is access to the token
+    //Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
